8af8c5b9f87a60bb1918f7068219690bcb919e9f
matias75772034 <matiasamir281@gmail.com>
2023-05-05 14:16:48 -0500

    components

---
+++ rick_and_morty/Client/src/componentes/prueba.js
@@ -0,0 +1,7 @@
+const alumnos = ["Jhona", "Feli", 'Lu']
+const sortAlumnos = alumnos.sort()
+console.log(sortAlumnos);
+
+const number = [12, 3, 5, 6, 1]
+const numberSort = number.sort()
+console.log(numberSort);
