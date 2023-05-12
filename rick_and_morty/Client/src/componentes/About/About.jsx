905a5ebf3d11263884f62bda7afad6ee25cc1cb2
matias75772034 <matiasamir281@gmail.com>
2023-05-12 18:49:46 -0500

    About

--- rick_and_morty/Client/src/componentes/About/About.jsx
+++ rick_and_morty/Client/src/componentes/About/About.jsx
@@ -3,6 +3,14 @@ const About = () => {
     return (
         <>
         <h2 className={style.title}>About...</h2>
+        
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus labore atque excepturi assumenda facere harum debitis magni beatae, officia, repellat modi nisi laboriosam aliquid voluptatum, dolor delectus odit cupiditate nihil! </p>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus labore atque excepturi assumenda facere harum debitis magni beatae, officia, repellat modi nisi laboriosam aliquid voluptatum, dolor delectus odit cupiditate nihil! </p>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus labore atque excepturi assumenda facere harum debitis magni beatae, officia, repellat modi nisi laboriosam aliquid voluptatum, dolor delectus odit cupiditate nihil! </p>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus labore atque excepturi assumenda facere harum debitis magni beatae, officia, repellat modi nisi laboriosam aliquid voluptatum, dolor delectus odit cupiditate nihil! </p>
         </>
     )
 }
