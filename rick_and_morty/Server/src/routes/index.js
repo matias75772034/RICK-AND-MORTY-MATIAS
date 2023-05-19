51a79a95a8254e67bea73acb2d41d2250bad8282
matias75772034 <matiasamir281@gmail.com>
2023-05-19 17:20:00 -0500

    routes

--- rick_and_morty/Server/src/routes/index.js
+++ rick_and_morty/Server/src/routes/index.js
@@ -1,13 +1,16 @@
 const express = require("express")
 const router = express.Router()
 
-const { getCharById } = require('../controllers/getCharById')
-const { login } = require('../controllers/login')
-const { postFav, deleteFav } = require('../controllers/handleFavorites')
-
-router.get("/character/:id", getCharById)
+const getCharById = require('../controllers/getCharById')
+const  login = require('../controllers/login')
+const postFav = require('../controllers/postFav')
+const postUser = require('../controllers/postUser')
+const deleteFav = require('../controllers/deleteFav')
 router.get("/login", login)
+router.post('/login', postUser)
 router.post("/fav", postFav)
 router.delete("/fav/:id", deleteFav)
+
+router.get("/character/:id", getCharById)
 
 module.exports = router
